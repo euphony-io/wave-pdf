d3055b93271213a5d5e0acd6aca0b95867aa7830
Park Hyejin <hyejin2371@gmail.com>
2022-09-09 11:21:40 +0900

    [Feature] Update viewer UI

---
+++ wave-ppt/eupi-ppt-viewer/src/main/java/com/euphony/eupi_ppt_viewer/EuPICodeEnum.kt
@@ -0,0 +1,6 @@
+package values
+
+enum class EuPICodeEnum(val code:Double) {
+    PREV_PAGE(18500.0),
+    NEXT_PAGE(20500.0)
+}
---
+++ wave-ppt/eupi-ppt-viewer/src/main/java/com/euphony/eupi_ppt_viewer/ViewerActivity.kt
@@ -0,0 +1,56 @@
+package com.euphony.eupi_ppt_viewer
+
+import android.media.Image
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import co.euphony.rx.EuRxManager
+import co.euphony.util.EuOption
+import com.euphony.eupi_ppt_viewer.ui.theme.WavepptTheme
+import values.EuPICodeEnum
+
+class ViewerActivity : ComponentActivity() {
+
+    private val TAG = "VIEWER"
+    private val rxManager = EuRxManager(EuOption.ModeType.EUPI)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        /*TODO: Load PDF */
+        /*TODO: PDF to Bitmap 이미지 변환 후 List화 */
+
+        setContent {
+
+        }
+
+    }
+}
+
+
+@Composable
+fun PageView(page: ImageBitmap) {
+
+    Image(
+        bitmap = page,
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+    )
+}
