d700b25434e60d7d12a1b4cc1cb380529ffdb832
Park Hyejin <hyejin2371@gmail.com>
2022-09-09 11:22:02 +0900

    [Feature] Update controller UI

---
+++ wave-ppt/eupi-ppt-controller/src/main/java/com/euphony/eupi_ppt_controller/ControllerActivity.kt
@@ -0,0 +1,43 @@
+package com.euphony.eupi_ppt_controller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.euphony.eupi_ppt_controller.ui.theme.WavepptTheme
+
+class ControllerActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WavepptTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WavepptTheme {
+        Greeting("Android")
+    }
+}
